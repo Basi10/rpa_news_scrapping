b09067f98f32454b5bb72e9b114cdc72d39d32a8
Basi10 <basilelbirru@gmail.com>
2024-04-30 20:51:43 +0300

    updated logging

--- script/exceptions.py
+++ script/exceptions.py
@@ -1,4 +1,4 @@
-from script import logger
+import robocorp.log as logger
 
 
 class ElementInteractionError(Exception):
@@ -7,8 +7,8 @@ class ElementInteractionError(Exception):
     def __init__(self, message="An error occurred."):
         self.message = message
         super().__init__(self.message)
-        self.logger = logger.setup_logger(__name__, './output/browser_action.log')
+        self.logger = logger
 
     def log_error(self, message: str):
         """Log the error message."""
-        self.logger.error(message)
+        self.logger.exception(message)
