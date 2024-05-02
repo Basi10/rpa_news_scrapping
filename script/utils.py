0f94007bba95ba788705191f8274ec74ca2dd010
Basi10 <basilelbirru@gmail.com>
2024-05-02 09:51:24 +0300

    documented code

--- script/utils.py
+++ script/utils.py
@@ -1,14 +1,30 @@
 import os
 import re
 import requests
-import uuid
-from datetime import datetime
+import logging
+import robocorp.log as logging
 from RPA.Excel.Files import Files
 from pathlib import Path
+from script.constants import (
+     Directories
+)
 
 
 def export_data_to_excel(name, data):
-    file_path = './output/{}.xlsx'.format(name)
+    """
+    Export data to an Excel file.
+
+    Args:
+        name (str): Name of the Excel file.
+        data (list): List of dictionaries representing rows of data.
+
+    Returns:
+        None
+    """
+    if not name.endswith(Directories.EXCEL_FILE_EXT):
+        name += Directories.EXCEL_FILE_EXT
+
+    file_path = os.path.join(Directories.EXCEL_DIRECTORY, name)
     excel = Files()
     if Path(file_path).is_file():
         excel.open_workbook(file_path)
@@ -19,25 +35,45 @@ def export_data_to_excel(name, data):
         excel.create_workbook(name)
         excel.create_worksheet(name=name, content=data, header=True)
         excel.save_workbook(file_path)
+    logging.info(f"Data exported to Excel file: {file_path}")
 
 
-def download_image(image_url, directory):
+def download_image(image_url, image_name, directory):
+    """
+    Download an image from a URL and save it to a directory.
+
+    Args:
+        image_url (str): URL of the image.
+        image_name (str): Name of the image file.
+        directory (str): Directory to save the image.
+
+    Returns:
+        str: Full path to the downloaded image file, or None if download failed.
+    """
     response = requests.get(image_url)
     if response.status_code == 200:
         if not os.path.exists(directory):
-            os.makedirs(directory)
-        unique_filename = str(uuid.uuid4())
-        filename = f"{unique_filename}_{datetime.now().strftime('%Y%m%d%H%M%S')}.jpg"
-        full_path = os.path.join(directory, filename)
+            os.makedirs(directory)    
+        full_path = os.path.join(directory, "{}.jpg".format(image_name.replace('/', '')))
         with open(full_path, 'wb') as f:
             f.write(response.content)
         print("Image downloaded and saved successfully:", full_path)
         return full_path
     else:
         print("Failed to download image")
+        return None
 
 
 def check_money(string):
+    """
+    Check if string contains money values or not.
+
+    Args:
+        string (str): Input string.
+
+    Returns:
+        bool: True if string contains money values, False otherwise.
+    """
     money_patterns = [
         r'\$\d+(\.\d+)?',
         r'\d+\s*(dollars|USD)',
@@ -48,6 +84,16 @@ def check_money(string):
 
 
 def count_keyword(string, keyword):
+    """
+    Count the number of occurrences of a keyword within a string.
+
+    Args:
+        string (str): Input string.
+        keyword (str): Keyword to search for.
+
+    Returns:
+        int: Number of occurrences of the keyword in the string.
+    """
     string_lower = string.lower()
     keyword_lower = keyword.lower()
     count = string_lower.count(keyword_lower)
